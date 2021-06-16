cea1022ab71c85a503cb3cf41a4902b777a832b0
MilhHdz <nhernandez@ethicsglobal.com>
2021-06-16 13:53:58 -0500

    nuevas vistas

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -4,4 +4,21 @@ class ArticulosController < ApplicationController
 
   def index
   end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
 end
